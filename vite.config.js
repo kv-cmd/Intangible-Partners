d1414b211510fd5f7ca1af3ce95ff408e760f09c
kv-cmd <kv@ateklo.com>
2025-10-28 23:55:10 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Intangible-Partners/", // important for GitHub Pages
+  base: "/Intangible-Partners/", // required for GitHub Pages
   build: {
     outDir: "dist",
   },
